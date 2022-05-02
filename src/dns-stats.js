71d66ff89faf839ce2d48a97f864ae772815de9d
YuriBorysiuk <65165493+YuriBorysiuk@users.noreply.github.com>
2022-05-03 00:33:36 +0300

    feat: completed dns-stats

--- src/dns-stats.js
+++ src/dns-stats.js
@@ -22,11 +22,20 @@ const { NotImplementedError } = require('../extensions/index.js');
  * }
  *
  */
-function getDNSStats(/* domains */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getDNSStats(domains) {
+	let result = {}
+	domains.forEach(item => {
+		let key = '';
+		item.split('.').reverse().forEach(item => {
+			key = `${key}.${item}`;
+			!result[key] 
+			? result[key] = 1 
+			: result[key] = result[key] + 1;
+		});
+	})
+	return result;
 }
 
 module.exports = {
-  getDNSStats
+	getDNSStats
 };
